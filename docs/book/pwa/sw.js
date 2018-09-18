e353804dd0debd5820b8a71301a389dd53a30854
nano <shinanome@qq.com>
2018-09-18 15:11:46 +0800

    update

--- docs/book/pwa/sw.js
+++ docs/book/pwa/sw.js
@@ -1,9 +1,10 @@
+var cacheVersion = 'v1.0.1';
 // 监听 service worker 的 install 事件
 self.addEventListener('install', function (event) {
   // 如果监听到了 service worker 已经安装成功的话，就会调用 event.waitUntil 回调函数
   event.waitUntil(
       // 安装成功后操作 CacheStorage 缓存，使用之前需要先通过 caches.open() 打开对应缓存空间。
-      caches.open('my-test-cache-v0.1').then(function (cache) {
+      caches.open(cacheVersion).then(function (cache) {
           // 通过 cache 缓存对象的 addAll 方法添加 precache 缓存
           return cache.addAll([
               './',
@@ -12,6 +13,7 @@ self.addEventListener('install', function (event) {
           ]);
       })
   );
+  // event.waitUntil(self.skipWaiting());
 });
 self.addEventListener('fetch', function (event) {
   event.respondWith(
@@ -45,11 +47,6 @@ self.addEventListener('fetch', function (event) {
       })
   );
 });
-// 安装阶段跳过等待，直接进入 active
-self.addEventListener('install', function (event) {
-  event.waitUntil(self.skipWaiting());
-});
-
 self.addEventListener('activate', function (event) {
   event.waitUntil(
       Promise.all([
@@ -61,7 +58,7 @@ self.addEventListener('activate', function (event) {
           caches.keys().then(function (cacheList) {
               return Promise.all(
                   cacheList.map(function (cacheName) {
-                      if (cacheName !== 'my-test-cache-v0.1') {
+                      if (cacheName !== cacheVersion) {
                           return caches.delete(cacheName);
                       }
                   })
