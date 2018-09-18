b00251c56bba9ca0b14a26bed8abb32353b5831b
nano <shinanome@qq.com>
2018-09-18 14:53:48 +0800

    update

--- docs/book/pwa/sw.js
+++ docs/book/pwa/sw.js
@@ -1,5 +1,5 @@
 // 监听 service worker 的 install 事件
-this.addEventListener('install', function (event) {
+self.addEventListener('install', function (event) {
   // 如果监听到了 service worker 已经安装成功的话，就会调用 event.waitUntil 回调函数
   event.waitUntil(
       // 安装成功后操作 CacheStorage 缓存，使用之前需要先通过 caches.open() 打开对应缓存空间。
@@ -15,7 +15,7 @@ this.addEventListener('install', function (event) {
       })
   );
 });
-this.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
       caches.match(event.request).then(function (response) {
           // 来来来，代理可以搞一些代理的事情
